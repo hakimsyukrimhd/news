c14636c42b6257ab9d4694f44606e5be9f9bd10b
Hakim Syukri <hakimsyukri.mhd@gmail.com>
2025-02-21 14:36:02 +0700

    updated news

--- kumparan/routes/news.js
+++ kumparan/routes/news.js
@@ -6,12 +6,13 @@ router.use("/category", categories);
 
 router.get("/", async (req, res) => {
   try {
-    const allOfNews = await pool.query("select * from news");
-    res.status(200).json(allOfNews.rows);
+    const result = await pool.query("select * from news");
+
+    res.status(200).json(result.rows);
   } catch (err) {
     console.error(err);
     res.status(500).json({
-      error: "Terdapat kesalahan pada server",
+      error: "There is an error on the server",
     });
   }
 });
@@ -20,13 +21,25 @@ router.get("/:slug", (req, res) => {
   const newsParams = req.params.slug;
 });
 
-router.post("/", (req, res) => {
+// baru sampai get pertama
+router.post("/", async (req, res) => {
   try {
-    // const 
+    const { title, body, imageUrl, date, userId, categoryId } = req.body;
+
+    if (!title && !body && !imageUrl && date && !userId && !categoryId) {
+      return res.status(404).json({
+        error: "Please provide at least one field to input",
+      });
+    }
+
+    const checkCategory = await pool.query("select * from categories where id = $1", [categoryId]);
+    if (checkCategory.rows.length === 0) {
+      const addCategory = await pool.query("insert into categories(name) values($1)", [categoryId]);
+    }
   } catch (err) {
     console.error(err);
     res.status(500).json({
-      error: "Terdapat kesalahan pada server",
+      error: "There is an error on the server",
     });
   }
 });
