d5f5d02e31c7eb42ac95f05a2b9628fe1e9eb9cd
Hakim Syukri <hakimsyukri.mhd@gmail.com>
2025-02-21 17:19:18 +0700

    update get slug

--- kumparan/routes/category.js
+++ kumparan/routes/category.js
@@ -13,8 +13,27 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:slug", (req, res) => {
-  const newsParams = req.params.slug;
+router.get("/:slug", async (req, res) => {
+  try {
+    const { slug } = req.params;
+
+    const query = "select * from categories where lower(name) = $1";
+    const values = [slug];
+    const result = await pool.query(query, values);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        error: "The category does not exist",
+      });
+    }
+
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: "There is an error on the server",
+    });
+  }
 });
 
 router.post("/", async (req, res) => {
