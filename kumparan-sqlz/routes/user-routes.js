9db14712f91773c5a1d2fdc0110ad5a080ff6264
Hakim Syukri <hakimsyukri.mhd@gmail.com>
2025-02-27 09:27:17 +0700

    delete everthing and restart again

--- kumparan-sqlz/routes/user-routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-router.get("/register", (req, res) => {
-  res.status(200).json({
-    message: "Welcome to Users",
-  });
-});
-
-module.exports = router;
